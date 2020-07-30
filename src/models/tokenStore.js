8ac3ba45e352ccb9d8acb34c2c1d2eb496127549
ethamitc <ethamitc@gmail.com>
2020-07-30 14:28:36 -0400

    Clean branch

--- src/models/tokenStore.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require('mongoose');
-
-const tokenStoreSchema = new Mongoose.Schema({
-	_id: String,
-	token: String,
-	expires: String
-});
-
-module.exports = Mongoose.model('TokenStore', tokenStoreSchema);
